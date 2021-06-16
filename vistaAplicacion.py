16458a812b60300124f822825c604ad73b8f03f3
ChrisLe7 <43857428+ChrisLe7@users.noreply.github.com>
2021-06-16 17:41:09 +0200

    Finalización de la aplicación de Estilos

--- vistaAplicacion.py
+++ vistaAplicacion.py
@@ -21,11 +21,7 @@ class ClasificacionDlg(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        self.colorEstablecidoBox = "background-color: rgb(218, 232, 236);"
-        #117, 220, 75 : 188, 240, 166 
-        
-        self.colorFondo = "background-color: rgb(188, 240, 166);"
-        self.estiloLabel = "font: bold italic 10pt; "
+        self.colorEstablecidoBox = """background-color: rgb(159, 168, 180 ); border: 2px solid; border-radius:5px; """
         widgetCentral = QWidget()
         
         cabeceraObservables = ['Observable', 'Valores Posibles']
@@ -49,18 +45,18 @@ class ClasificacionDlg(QMainWindow):
         
         #Listado de las posibles hipotesis que se puedan dar
 
-        self.labelHipotesisL=QLabel("Posibles Hipotesis",self)#Creamos un listwidget para las posibles hipotesis
+        self.labelHipotesisL=QLabel("Posibles Hipótesis",self)#Creamos un listwidget para las posibles hipotesis
         self.listWidgetHipotesis = QListWidget()#Lista de hipotesis
         #self.rellenarPosiblesHipotesis()
         
         #ListWidget para el diagnostico
-        self.labelDiagnosticoL=QLabel("Diagnostico Encontrado",self)
+        self.labelDiagnosticoL=QLabel("Diagnóstico Encontrado",self)
         self.listWidgetDiagnosticos = QListWidget()#Lista de diagnosticos
         
         
         #Texto de explicación del diagnostico
           
-        self.labelExplicacionL=QLabel("Explicacion",self)
+        self.labelExplicacionL=QLabel("Explicación",self)
         self.PlainTextEditExplicacion = QPlainTextEdit("Todavía no se ha realizado al diagnostico")#Cuadro de texto de la explicacion 
           
         #Botones
@@ -167,28 +163,58 @@ class ClasificacionDlg(QMainWindow):
         self.setFixedSize(1318, 750)
         self.setWindowIcon(QIcon('imagenes/Logo_UCO.png'))
        
-        stlButton = """QPushButton {
-        background-color: palegoldenrod;
-        border-width: 2px;
-        border-color: darkkhaki;
-        border-style: solid;
-        border-radius: 5;
-        padding: 3px;
-        min-width: 9ex;
-        min-height: 2.5ex;
-        }"""
-        QApplication.instance().setStyleSheet(stlButton)
-        
+       
+         
         self.establecerColores()
         self.center()
         self.show()
 
     def establecerColores(self):
-        self.tablaFallos.setStyleSheet(self.colorEstablecidoBox)
-        self.tablaObservables.setStyleSheet(self.colorEstablecidoBox )
-        self.listWidgetHipotesis.setStyleSheet(self.colorEstablecidoBox )
-        self.listWidgetDiagnosticos.setStyleSheet(self.colorEstablecidoBox )
-        self.PlainTextEditExplicacion.setStyleSheet(self.colorEstablecidoBox )
+        
+        
+        #117, 220, 75 : 188, 240, 166 
+        
+        self.colorFondo = "background-color: #513d47;"
+        self.estiloLabel = "font: bold italic 10pt; color: #d3d7d2"
+        
+        stlTabla = """
+        QHeaderView {
+             background-color:rgb(98, 115, 71 );
+            font: bold 10pt;
+        }
+        
+        QHeaderView::section{
+            background:rgb(98, 115, 71  );
+        }
+        
+        QTableWidget {
+            background-color: rgb(168, 182, 148 );
+            
+        }
+        
+        QComboBox {
+            background-color: rgb(168, 182, 148 );
+        }
+    
+     
+        """
+        stlLista = """
+            QListWidget {
+                background-color: rgb(168, 182, 148 );
+            }
+        """
+        
+        stlTextPlain = """
+            QPlainTextEdit {
+                background-color: rgb(168, 182, 148 );
+            }
+        """
+        self.tablaFallos.setStyleSheet(stlTabla)
+        
+        self.tablaObservables.setStyleSheet(stlTabla)
+        self.listWidgetHipotesis.setStyleSheet(stlLista )
+        self.listWidgetDiagnosticos.setStyleSheet(stlLista)
+        self.PlainTextEditExplicacion.setStyleSheet(stlTextPlain)
         self.setStyleSheet(self.colorFondo)
         
         self.labelFallosA.setStyleSheet(self.estiloLabel)
@@ -196,6 +222,8 @@ class ClasificacionDlg(QMainWindow):
         self.labelHipotesisL.setStyleSheet(self.estiloLabel)
         self.labelDiagnosticoL.setStyleSheet(self.estiloLabel)
         self.labelExplicacionL.setStyleSheet(self.estiloLabel)
+        self.colorEstablecidoBoton = """background-color: rgb(159, 168, 180 ); border: 2px solid; border-radius:5px; padding: 0.5em; """
+        self.diagnosticaButton.setStyleSheet(self.colorEstablecidoBoton)
 
     def center(self):
         
